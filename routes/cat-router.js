829ddf6869869e077f70e2c6663a2ca576126e70
Darryl Davis <ddavis1306@gmail.com>
2017-10-30 14:39:39 -0700

    get workinggit add .

--- routes/cat-router.js
+++ routes/cat-router.js
@@ -7,6 +7,13 @@ const Cat = require(__dirname + '/../models/cat.js');
 
 const catRouter = module.exports = express.Router();
 //explain line ^
+catRouter.get('/cats', (req, res, next) => {
+  let findCat = {} ;
+  Cat.find(findCat)
+  .then(cat => res.send(cat))
+  .catch(err => next({statusCode: 500, error: err}));
+});
+
 catRouter.post('/cats', jsonParser, (req, res, next) =>{
   //parse the json coming in^
   let newCat = new Cat(req.body);
@@ -14,5 +21,11 @@ catRouter.post('/cats', jsonParser, (req, res, next) =>{
   //.save() returns a promise^
     .then(data => res.send(data))
     //getting the data from the db and sendinging it back a response^
+    .catch(err => next({statusCode: 500, message: 'error creating cat', error: err}));
+});
+catRouter.put('/cats', jsonParser, (req, res, next) =>{
+  let newCat = new Cat(req.body);
+  newCat.save()
+    .then(data => res.send(data))
     .catch(err => next({statusCode: 500, message: 'error creating cat', error: err}));
 });
