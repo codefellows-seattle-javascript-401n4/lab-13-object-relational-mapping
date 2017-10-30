6720ac91dc0eb2dbfa75b1d91fc532c1f20dae47
Darryl Davis <ddavis1306@gmail.com>
2017-10-30 09:11:36 -0700

    cat model and scafolding

---
+++ models/cat.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const catSchema = new mongoose.Schema({
+  name: {type: String, required: true, unique: true},
+  birthday: {type: Date, default: Date.now},
+  favoriteDrug: {type: String, default: 'cat nip'},
+  favoriteFood: {type: String, default: 'meow mix'},
+
+});
+module.exports = mongoose.model('Cat', catSchema);
---
+++ routes/cat-router.js
@@ -0,0 +1,9 @@
+// 'use strict';
+//
+// const express = require('express');
+// const jsonParser = require('body-parser').json();
+// //This is where we pull in json data^
+// const Cat = require(__dirname + '/../models/cat.js');
+//
+// const catRouter = module.exports = express.Router();
+// //explain line ^
